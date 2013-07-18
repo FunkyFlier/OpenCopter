40b80f1205a53ff9400530c7940081b2cdd2e9f3
Michael Baker <mikeshub1@gmail.com>
2013-07-18 11:15:59 -0500

    Add motors 5 - 8

--- openCopter/Motors.ino
+++ openCopter/Motors.ino
@@ -3,15 +3,17 @@ void MotorInit(){
   DDRH |= B00001000;
 
   
-  // Init PWM Timer 3                                       
-  // WGMn1 WGMn2 WGMn3  = Mode 14 Fast PWM, TOP = ICRn ,Update of OCRnx at BOTOM
-  TCCR3A = (1<<WGM31)|(1<<COM3A1)|(1<<COM3B1)|(1<<COM3C1);  // Clear OCnA/OCnB/OCnC on compare match, set OCnA/OCnB/OCnC at BOTTOM (non-inverting mode)
-  TCCR3B = (1<<WGM33)|(1<<WGM32)|(1<<CS31);                 // Prescaler set to 8, that gives us a resolution of 0.5us
-  ICR3 = PERIOD;                                // Clock_speed / ( Prescaler * desired_PWM_Frequency) #defined above.  
+  TCCR3A = (1<<WGM31)|(1<<COM3A1)|(1<<COM3B1)|(1<<COM3C1);  
+  TCCR3B = (1<<WGM33)|(1<<WGM32)|(1<<CS31);               
+  ICR3 = PERIOD;   
 
-  TCCR4A = (1<<WGM41)|(1<<COM4A1);
+  TCCR4A = (1<<WGM41)|(1<<COM4A1)|(1<<COM4B1)|(1<<COM4C1);
   TCCR4B = (1<<WGM43)|(1<<WGM42)|(1<<CS41);
-  ICR4 = PERIOD;
+  ICR4 = PERIOD;  
+
+  TCCR1A = (1<<WGM11)|(1<<COM1A1)|(1<<COM1B1);
+  TCCR1B = (1<<WGM13)|(1<<WGM12)|(1<<CS11);
+  ICR1 = PERIOD;
   
   Motor1WriteMicros(1000);//set the output compare value
   Motor2WriteMicros(1000);
